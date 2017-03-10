f6ef4f86ced311f910ea7f70a83bed43a191d532
yan96in <yan96in@163.com>
2017-03-10 10:50:16 +0800

    clone

---
+++ test/clone/CloneTest.java
@@ -0,0 +1,18 @@
+package clone;
+
+public class CloneTest {
+
+	public static void main(String[] args) {
+		try {
+			Employee original = new Employee("John Q. Public", 50000);
+			original.setHireDay(2000, 1, 1);
+			Employee copy = original.clone();
+			copy.raiseSalary(10);
+			System.out.println("original=" + original);
+			System.out.println("copy=" + copy);
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
